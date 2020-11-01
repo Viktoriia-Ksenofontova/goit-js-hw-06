fe1aa00e9eb2bcde6028b936811853425f2cf870
Viktoriia Ksenofontova <glory_viktory@hotmail.com>
2020-11-01 16:33:02 +0200

    done!

---
+++ script/task-6-9.js
@@ -0,0 +1,55 @@
+const getNamesSortedByFriendsCount = (array) => {
+ let newArray = [...array]
+   .sort((a, b) => {
+     if (a.friends.length < b.friends.length) {
+       return - 1;
+     }
+     if (a.friends.length > b.friends.length) {
+       return 1;
+     }
+     return 0;
+   })
+    .map(({ name }) => name);
+  return newArray;
+};
+
+const users = [
+  {
+    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace", "Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37,
+  },
+  {
+    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "green",
+    friends: ["Briana Decker"],
+    isActive: true,
+    balance: 3821,
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34,
+  },
+  {
+    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27,
+  }
+];
+
+console.log(getNamesSortedByFriendsCount(users));
